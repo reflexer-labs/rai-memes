a92c912d5880b98157dc0fc0a80bcbf2b6be4d46
mstfash <m.ashraf.hegab@gmail.com>
2021-10-19 20:10:12 +0200

    fix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,20 +1,10 @@
-const _ = require('lodash');
-
-// cleanup html before render
-exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+exports.onPreRenderHTML = ({ replaceHeadComponents, getHeadComponents }) => {
   const headComponents = getHeadComponents();
-  replaceHeadComponents(omitDeep(headComponents, ['data-react-helmet']));
-};
-
-const omitDeep = (collection, excludeKeys) =>
-  _.cloneDeepWith(collection, (value) => {
-    if (value && typeof value === 'object') {
-      for (const key of excludeKeys) {
-        try {
-          delete value[key];
-        } catch (_) {
-          // console.log("ignore", _);
-        }
-      }
+  console.log(headComponents);
+  headComponents.forEach((head) => {
+    if (head.props && head.props['data-react-helmet']) {
+      delete head.props['data-react-helmet'];
     }
   });
+  replaceHeadComponents(headComponents);
+};
