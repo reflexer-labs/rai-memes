ea33196b7f821c6a15c037233c1afd3a1f808f4b
mstfash <m.ashraf.hegab@gmail.com>
2021-11-01 20:03:47 +0200

    fix memes

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -4,77 +4,13 @@ import { Helmet } from 'react-helmet';
 const BASE_URL = 'https://memes.reflexer.finance';
 
 const SEO = ({ image: imgUrl }) => {
-  console.log(imgUrl, 'imgUrl from seo');
   return (
     <Helmet>
+      <link rel="icon" href={`${BASE_URL}/images/icon.png`} />
       <link
-        rel="apple-touch-icon"
-        sizes="57x57"
-        href={`${BASE_URL}/images/favicon/apple-icon-57x57.png`}
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="60x60"
-        href={`${BASE_URL}/images/favicon/apple-icon-60x60.png`}
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="72x72"
-        href={`${BASE_URL}/images/favicon/apple-icon-72x72.png`}
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="76x76"
-        href={`${BASE_URL}/images/favicon/apple-icon-76x76.png`}
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="114x114"
-        href={`${BASE_URL}/images/favicon/apple-icon-114x114.png`}
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="120x120"
-        href={`${BASE_URL}/images/favicon/apple-icon-120x120.png`}
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="144x144"
-        href={`${BASE_URL}/images/favicon/apple-icon-144x144.png`}
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="152x152"
-        href={`${BASE_URL}/images/favicon/apple-icon-152x152.png`}
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href={`${BASE_URL}/images/favicon/apple-icon-180x180.png`}
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href={`${BASE_URL}/images/favicon/android-icon-192x192.png`}
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href={`${BASE_URL}/images/favicon/favicon-32x32.png`}
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href={`${BASE_URL}/favicon-96x96.png`}
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href={`${BASE_URL}/images/favicon/favicon-16x16.png`}
+        rel="shortcut icon"
+        type="image/ico"
+        href={`${BASE_URL}/images/icon.png`}
       />
       <link
         rel="image_src"
